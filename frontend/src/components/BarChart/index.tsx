3f715d0f0749bd2b40845682bf60f14439a7f051
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-11-07 23:28:09 -0300

    Fix import

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import axios from 'axios';
-import { SaleSuccess, SaleSum } from 'types/sale';
+import { SaleSuccess} from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 import { round } from 'utils/format';
 
